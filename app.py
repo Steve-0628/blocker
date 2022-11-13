3426c4d7eb9a6ede82f97c0365aad631ee4254b5
stesan <github@hmpf.club>
2022-11-13 22:12:05 +0900

    bb

--- app.py
+++ app.py
@@ -27,9 +27,10 @@ def login():
     if os.environ.get("PORT"):
         callback = "https://blocker.hmpf.club/oauth/"
     if "hisubway" in flask.request.args:
-        callback = "http://bulkblock.hisubway.online/oauth/subway/"
+        callback = "http://bb.hisubway.online/oauth/subway/"
     elif "blocklist" in flask.request.args:
         callback = "https://blocker.hmpf.club/oauth/blocklist/"
+    
     oauth = OAuth1Session(CK, CS, callback_uri=callback)
     url = "https://api.twitter.com/oauth/request_token"
     req = oauth.fetch_request_token(url)
